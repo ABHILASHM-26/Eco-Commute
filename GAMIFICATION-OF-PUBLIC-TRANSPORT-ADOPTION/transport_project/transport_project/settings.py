b2a69af66bbcf294d2c87deea5f63425ee590c14
ABHILASH MELLACHERUVU <abhilashm9585@gmail.com>
2025-07-24 00:00:04 +0530

    Update settings.py

--- GAMIFICATION-OF-PUBLIC-TRANSPORT-ADOPTION/transport_project/transport_project/settings.py
+++ GAMIFICATION-OF-PUBLIC-TRANSPORT-ADOPTION/transport_project/transport_project/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('s1#@4bnl*@sd3mlc99tot3yow+7%sgd_!c8#x(^6zwk0$30%xu')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
+
+
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
