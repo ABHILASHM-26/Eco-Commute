6567870e0a6c3a2327a5083eb30b44d64dd16881
ABHILASH MELLACHERUVU <abhilashm9585@gmail.com>
2025-07-28 03:07:20 +0530

    Update BadgeSystem.js

--- GAMIFICATION-OF-PUBLIC-TRANSPORT-ADOPTION/frontend/frontend/src/components/BadgeSystem.js
+++ GAMIFICATION-OF-PUBLIC-TRANSPORT-ADOPTION/frontend/frontend/src/components/BadgeSystem.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import '../App.css'; // Assuming you have some basic styles
+import '../App.css'; 
 
 const BadgesPage = () => {
   return (
     <div className="badges-page">
       <h1 className="page-title">Badges</h1>
 
-      {/* Add a nice description text */}
+      {}
       <p className="badge-description">
         Congratulations Nandha Gopal on your achievements! 🌟 Earn more badges by riding and saving the environment!
       </p>
 
-      {/* Add some badge images */}
+      {}
       <div className="badges-container">
         <img
           src="https://images.squarespace-cdn.com/content/v1/5a9592c9cc8fed6981ecd0d7/5cd8e55b-edba-4fda-a391-104e9f375d0d/Warrior_Full.png"
@@ -25,7 +25,7 @@ const BadgesPage = () => {
         />
       </div>
 
-      {/* Later you can map actual user badges dynamically here */}
+      {}
     </div>
   );
 };
